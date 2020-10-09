e8c39a05427b2af7487e90b201738a8a2032bec2
ldqLDQ <10970329+ldqLDQ@users.noreply.github.com>
2020-10-09 21:50:38 +0800

    Update
    flask-restful test

--- app.py
+++ app.py
@@ -2,17 +2,26 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class UserAPI(Resource):
+    def get(self, id):
+        return {"user": id}
+
+    def put(self, id):
+        pass
+
+    def delete(self, id):
+        pass
 
 
-@app.route('/user')
-def user():
-    return 'USR!'
+class IndexView(Resource):
+    def get(self):
+        return {"hello":"world"}
 
 
-if __name__ == '__main__':
-    app.run()
+api.add_resource(UserAPI, '/users/<int:id>', endpoint='/user')
+api.add_resource(IndexView,'/',endpoint='index')
+#if __name__ == '__main__':
+#    app.run()
